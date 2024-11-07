56664be2429879afd58fddc6dd3047d8338a51fe
Boris <bgrebennikovv@gmail.com>
2024-11-07 18:28:49 +0400

    Task 1

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -1,7 +1,16 @@
 package com.github.bgrebennikovv;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.printf("Dog: %s\nCat: %s\nPaper: %s", dog, cat, paper);
+
     }
+
 }

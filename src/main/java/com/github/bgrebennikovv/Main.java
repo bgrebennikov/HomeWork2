7891b6c7db986c30996b9b0c36342e2f27b1e623
Boris <bgrebennikovv@gmail.com>
2024-11-07 18:32:40 +0400

    Task 2

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -9,7 +9,14 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.printf("Dog: %s\nCat: %s\nPaper: %s", dog, cat, paper);
+        System.out.printf("Task 1\nDog: %s\nCat: %s\nPaper: %s\n\n", dog, cat, paper);
+
+        // Task 2
+        dog += 4;
+        cat += 4;
+        paper += 4;
+
+        System.out.printf("Task 2, New values:\nDog: %s\nCat: %s\nPaper: %s\n\n", dog, cat, paper);
 
     }
 
